0a4d1f12192f1a381bfb497f867fd2c3f28612f3
Richard Gaun <richard@gaun.eu>
2014-09-01 16:12:55 +0200

    new files

--- www/js/index.js
+++ www/js/index.js
@@ -17,9 +17,26 @@
  * under the License.
  */
 var app = {
+		
+		findByName: function() {
+		    var self = this;
+		    this.store.findByName($('.search-key').val(), function(employees) {
+		        $('.employee-list').html(self.employeeLiTpl(employees));
+		    });
+		},
+		
+		renderHomeView: function() {
+		    $('body').html(this.homeTpl());
+		    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+		},
+		
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        
+        // 
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
     // Bind Event Listeners
     //
